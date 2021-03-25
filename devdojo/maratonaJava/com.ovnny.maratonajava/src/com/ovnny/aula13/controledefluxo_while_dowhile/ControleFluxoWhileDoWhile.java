a43b226c64a463ad53679a2035290db0089b8c0d
ovnny <viniciusricardo.ferrera@gmail.com>
2021-03-25 13:23:50 -0300

    Estudo de caso do controle de fluxo 'while' e 'do while'

---
+++ devdojo/maratonaJava/com.ovnny.maratonajava/src/com/ovnny/aula13/controledefluxo_while_dowhile/ControleFluxoWhileDoWhile.java
@@ -0,0 +1,26 @@
+package com.ovnny.aula13.controledefluxo_while_dowhile;
+
+public class ControleFluxoWhileDoWhile {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+        while (contador < 10) {
+            System.out.println(++contador);
+            // 1 2 3 4 5 6 7 8 9 10
+            // não imprime o "0" pois o operador ++ vem antes da
+            // primeira condição ser executada (pré-fixado)
+        }
+
+        int contador2 = 0;
+        while (contador2 < 10) {
+            System.out.println(contador2++);
+            // 0 1 2 3 4 5 6 7 8 9 10
+            // imprime o "0" pois o operador ++ é pós fixado
+        }
+
+        do {
+            System.out.println(contador--);
+        } while (contador > 0);
+    }
+}
